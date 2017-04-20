a2b275200622e1be6db8c9f4a0b361f8642dcd7b
Jesus Isaac Sandoval Flores <sfji_05@hotmail.com>
2017-04-20 17:41:05 +0000

    yes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was successfully created"
+            flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
         else
             
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
     
     def update 
         if @article.update(article_params)
-            flash[:notice]= "Article Updated Successfully"
+            flash[:success]= "Article Updated Successfully"
             redirect_to article_path(@article)
         else
             render 'edit'
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
     
     def destroy
         @article.destroy
-        flash[:notice] = "Article has been deleted"
+        flash[:danger] = "Article has been deleted"
         redirect_to articles_path
     end
     
