f3a6b204f7bbfc38bf5b7a3672ce5693fbc4c1d1
Jesus Isaac Sandoval Flores <sfji_05@hotmail.com>
2017-05-02 16:59:33 +0000

    signup and edit user added to the Isaac's blog

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     get 'signup', to: 'users#new'
     # two ways to create a route to the actions of users 
     #1.- create a path to submit users 
-    post 'users', to: 'users#create'
+    #post 'users', to: 'users#create'
     #2.- put resources and de name of the resource 
     resources :users, except: [:new]
     #In this case we don't put the new route because we already have it
